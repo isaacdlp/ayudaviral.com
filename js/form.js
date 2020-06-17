a9be4403df0a2ac9079adab7dadfecaadaa9f0bb
Isaac de la Peña <admin@agoraeafi.com>
2020-06-17 10:51:22 -0700

    Form messages improved

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 function form_msg(status, message) {
     var formMessages = $('#form-messages');
+    $(formMessages).css('display', 'block');
     $(formMessages).removeClass('bg-danger');
     $(formMessages).removeClass('bg-warn');
     $(formMessages).removeClass('bg-success');
@@ -20,29 +21,29 @@ $(function() {
         // Serialize the form data.
         var formData = $(form).serialize();
 
-        form_msg('bg-warn', 'Sending message. Just a second');
+        form_msg('bg-warn', 'Enviando el mensaje. Un momento');
         // Submit the form using AJAX.
         $.ajax({
             type: 'POST',
             url: $(form).attr('action'),
             data: formData
         })
-            .done(function(response) {
-                if (response.status == "success") {
-                    // Make sure that the formMessages div has the 'success' class.
-                    form_msg('bg-success', 'Message sent. We will be in touch shortly');
-                } else {
-                    // Make sure that the formMessages div has the 'success' class.
-                    form_msg('bg-danger', 'Message not sent. Please try again');
-                }
+        .done(function(response) {
+            if (response.status == "success") {
+                // Make sure that the formMessages div has the 'success' class.
+                form_msg('bg-success', 'Mensaje enviado. Responderemos en breve');
+            } else {
+                // Make sure that the formMessages div has the 'success' class.
+                form_msg('bg-danger', 'Mensaje no enviado. Por favor reinténtelo');
+            }
 
-                // Clear the form.
-                $('#name, #email, #message').val('');
-            })
-            .fail(function(data) {
-                // Make sure that the formMessages div has the 'error' class.
-                form_msg('bg-danger', 'Oops! An error occured and your message could not be sent.');
-            });
+            // Clear the form.
+            $('#name, #email, #message').val('');
+        })
+        .fail(function(data) {
+            // Make sure that the formMessages div has the 'error' class.
+            form_msg('bg-danger', 'Oops! Hubo un error y no se pudo enviar su mensaje');
+        });
 
     });
 
